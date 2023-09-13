b06c4effccd74cad8a5756d3872f05ca0feb175e
Mathish12 <144827093+Mathish12@users.noreply.github.com>
2023-09-13 18:05:03 +0530

    Main1.py

--- Unit-1 Challenge/Main1.py
+++ Unit-1 Challenge/Main1.py
@@ -1,1 +1,10 @@
+#Unit 1 challenge 1
+def fact_rect(n):
+ if n==0 or n==1:
+  return 1
+ else:
+   return n*fact_rect(n-1)
+number=int(input("Enter the Number : "))
+res =fact_rect(number)
+print("The factorial of {} is {}".format(number,res))
 
